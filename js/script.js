5ef190058c61cc3ebdcc84e6e8b4aad776c72628
S.mohsen <1mr.rooter3@gmail.com>
2023-10-19 13:13:25 +0330

    add script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-﻿
+﻿let body = document.body;
+let contextMenu = document.getElementById("contextMenu");
+body.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+  contextMenu.style.visibility = "visible";
+  contextMenu.style.opacity = "1";
+  let s = (contextMenu.style.transform =
+    "translate(" + e.pageX + "px," + e.pageY + "px)");
+  console.log(e.pageX, e.pageY);
+  console.log(s);
+});
+body.addEventListener("click", (e) => {
+    e.preventDefault();
+    contextMenu.style.visibility = "hidden";
+    contextMenu.style.opacity = "0";
+  });
